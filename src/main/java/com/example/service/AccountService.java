21f3ddbc3524b2b3961b76b1eec65e5262f37d5a
Loy Pereyra <loypereyra03@gmail.com>
2025-05-10 04:32:14 +0000

    c1

--- src/main/java/com/example/service/AccountService.java
+++ src/main/java/com/example/service/AccountService.java
@@ -1,4 +1,29 @@
 package com.example.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.entity.Account;
+import com.example.repository.AccountRepository;
+
+@Service
 public class AccountService {
+
+    private AccountRepository accountRepository;
+
+    @Autowired
+    public AccountService (AccountRepository accountRepository) {
+
+        this.accountRepository = accountRepository;
+    }
+
+    public Account registerAccount (Account account) {
+
+        if (account.getUsername().length() >= 4 && !accountRepository.findByUsername(account.getUsername()).isPresent()) {
+
+            return accountRepository.save(account);
+        }
+
+        return null;
+    }
 }
